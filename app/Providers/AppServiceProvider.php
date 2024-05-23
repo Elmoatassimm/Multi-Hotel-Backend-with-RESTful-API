54e7a9addbb738375f1e76ccf762aec02aa934e3
elmoatassim_billah <mouatassemben2004@gmail.com>
2024-05-23 22:51:23 +0100

    Define gate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
+
 use Illuminate\Support\ServiceProvider;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('isAdmin', function (User $user) {
+            return $user->role == 'admin';
+        });
+
+        Gate::define('isUser', function (User $user) {
+            return $user->role == 'guest';
+        });
     }
 }
